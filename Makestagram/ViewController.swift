399688e3bdaa3b723e149a013117c0e6d944de28
Joe Suzuki <Joes516@users.noreply.github.com>
2017-07-05 15:36:01 -0400

    Added Firebase

---
+++ Makestagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Makestagram
+//
+//  Created by Joe Suzuki on 7/5/17.
+//  Copyright © 2017 JoeSuzuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
