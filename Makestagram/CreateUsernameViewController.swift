7e6ae59bc271ea820f6125616f638f9a801f93e0
Joe Suzuki <Joes516@users.noreply.github.com>
2017-07-07 14:10:33 -0400

    finished connecting username to email account

--- Makestagram/CreateUsernameViewController.swift
+++ Makestagram/CreateUsernameViewController.swift
@@ -28,23 +28,25 @@ class CreateUsernameViewController: UIViewController {
     
     
     @IBAction func nextButtonTapped(_ sender: UIButton) {
-        // 1
+        // 1 First we guard to check that a FIRUser is logged in and that the user has provided a username in the text field.
         guard let firUser = Auth.auth().currentUser,
             let username = usernameTextField.text,
             !username.isEmpty else {return}
-        // 2
+        // 2 We create a dictionary to store the username the user has provided inside our database
         let userAttrs = ["username": username]
         
-        //3 
+        //3 We specify a relative path for the location of where we want to store our data
         let ref = Database.database().reference().child("users").child(firUser.uid)
-        // 4
+        // 4 We write the data we want to store at the location we provided in step 3
         ref.setValue(userAttrs) { (error, ref) in
             if let error = error {
                 assertionFailure(error.localizedDescription)
                 return
             }
-        // 5
-            ref.obser
+        // 5 We read the user we just wrote to the database and create a user from the snapshot
+            ref.observeSingleEvent(of: .value, with: { (snapshot) in
+                let user = User(snapshot: snapshot)
+            })
         
         }
         
