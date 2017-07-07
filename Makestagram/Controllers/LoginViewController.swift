2ba733e174ff9c5f864c99b521bb00d8082aff86
Joe Suzuki <Joes516@users.noreply.github.com>
2017-07-07 12:07:51 -0400

    connected custom username to firebase signin

--- Makestagram/Controllers/LoginViewController.swift
+++ Makestagram/Controllers/LoginViewController.swift
@@ -52,16 +52,20 @@ extension LoginViewController: FUIAuthDelegate {
         
         let userRef = Database.database().reference().child("users").child(user.uid)
         // 3
-        userRef.observeSingleEvent(of: .value, with: { (snapshot) in
+        userRef.observeSingleEvent(of: .value, with: { [unowned self] (snapshot) in
             if let user = User(snapshot: snapshot) {
                 print("Welcome back, \(user.username).")
             } else {
-                print("New user!")
+                self.performSegue(withIdentifier: "toCreateUsername", sender: self)
             }
         })
         
         // ...
     }
+    
+    
+    
+    
 }
 
 
--- Makestagram/CreateUsernameViewController.swift
+++ Makestagram/CreateUsernameViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
 
 class CreateUsernameViewController: UIViewController {
     
@@ -26,6 +28,28 @@ class CreateUsernameViewController: UIViewController {
     
     
     @IBAction func nextButtonTapped(_ sender: UIButton) {
+        // 1
+        guard let firUser = Auth.auth().currentUser,
+            let username = usernameTextField.text,
+            !username.isEmpty else {return}
+        // 2
+        let userAttrs = ["username": username]
+        
+        //3 
+        let ref = Database.database().reference().child("users").child(firUser.uid)
+        // 4
+        ref.setValue(userAttrs) { (error, ref) in
+            if let error = error {
+                assertionFailure(error.localizedDescription)
+                return
+            }
+        // 5
+            ref.obser
+        
+        }
+        
+        
+        
     }
 
 
